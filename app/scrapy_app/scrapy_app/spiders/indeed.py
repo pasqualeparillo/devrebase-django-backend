4713a2635abd260d6a47103062abb6767d1d1bb2
patp0513 <patparillo@gmail.com>
2019-10-29 09:54:03 -0400

    scrapy spider setup, need to test.

--- app/scrapy_app/scrapy_app/spiders/indeed.py
+++ app/scrapy_app/scrapy_app/spiders/indeed.py
@@ -3,35 +3,22 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-
 class IndeedSpider(CrawlSpider):
     name = 'indeed'
     start_urls = [
-
-        'http://quotes.toscrape.com/',
-
+        'https://www.indeed.com/jobs?q=software+engineer&l=',
     ]
-
-
-
     def parse(self, response):
-
-        for quote in response.css("div.quote"):
-
+        for job in response.css(".jobsearch-SerpJobCard"):
             yield {
-
-                'text': quote.css("span.text::text").extract_first(),
-
-                'author': quote.css("small.author::text").extract_first(),
-
-                'tags': quote.css("div.tags > a.tag::text").extract()
-
+                'job_company': ,
+                'job_title': job.css("div.title > a::attr(title)").extract_first(),
+                'job_body': job.css("div.summary::text"),
+                'job_url': job.css("div.title > a::attr(href)"),
+                'job_source': job.setdefault('indeed'),
+                'job_location': job.css("div > div::attr(loc)"),
             }
 
-
-
-        next_page_url = response.css("li.next > a::attr(href)").extract_first()
-
+        next_page_url = response.css("div.pagination > a::attr(href)").extract_first()
         if next_page_url is not None:
-
             yield scrapy.Request(response.urljoin(next_page_url))
